a70575fefff72d0dfd2ac24f5a2a38cffe61ff73
John Colton <jcolton96@gmail.com>
2022-09-26 20:24:51 +0100

    Lesson 38

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,20 @@
-import './ExpenseItem.css'
+import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const year = props.date.getFullYear();
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+
   return (
-    <div className='expense-item'>
-      <div>September 23rd 2022</div>
-      <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>£299.99</div>
+    <div className="expense-item">
+      <div>
+        <div>{year}</div>
+        <div>{month}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">£{props.amount}</div>
       </div>
     </div>
   );
